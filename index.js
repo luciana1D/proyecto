e6f240b33fba8bb35729d90905c081027b2fda1f
luciana1D <renibenitez52@gmail.com>
2022-09-01 14:36:35 -0300

    primer cambio

---
+++ index.js
@@ -0,0 +1,51 @@
+const tareas = [];
+let sacar = "";
+
+function ingresarTarea() {
+    let ingresar;
+    do {
+        ingresar = prompt("Ingresar una tarea").toUpperCase();
+    } while (ingresar === "" || ingresar === null);
+    tareas.push(ingresar);
+    let conf = confirm(tareas.join("\n"));
+    if (conf === true) {
+        nuevaLista();
+    }
+}
+
+function nuevaLista() {
+    let newList;
+
+    do {
+        newList = parseInt(
+            prompt(`1)Ingresar nueva tarea
+2)Visualizar tareas
+3)Eliminar tarea`)
+        );
+        switch (newList) {
+            case 1:
+                ingresarTarea();
+                break;
+            case 2:
+                tareas.forEach((el) => confirm(el));
+                nuevaLista();
+                break;
+            case 3:
+                sacar = prompt(`Cual tarea deseas eliminar de tu lista?
+${tareas.join("\n")} `).toUpperCase();
+                eliminar(sacar);
+                nuevaLista();
+                break;
+        }
+    } while (isNaN(newList) || newList === "" || (newList != 1 && newList != 2 && newList != 3));
+}
+const eliminar = (el) => {
+    let index = tareas.indexOf(el);
+    if (index != -1) {
+        tareas.splice(index, 1);
+    }
+};
+
+ingresarTarea();
+nuevaLista();
+eliminar();
